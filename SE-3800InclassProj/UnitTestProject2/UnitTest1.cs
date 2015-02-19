49fb46c0a40d1ca1e4043963f20e3866ec6aeddf
Brandon Jackson <jacksonbt@msoe.edu>
2015-02-19 16:08:52 -0600

    really added tests and travis

---
+++ SE-3800InclassProj/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.CodeDom;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using SE_3800InclassProj.Controllers.API;
+using SE_3800InclassProj.Models;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTestProject2
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var NC = new NumbersController();
+            Number max = NC.GetMaxNumber();
+            Assert.AreEqual(max.number, 68);
+        }
+    }
+}
