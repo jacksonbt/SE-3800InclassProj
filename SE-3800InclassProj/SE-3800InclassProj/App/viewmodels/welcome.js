06b405502a89247a359fd2c7da962c7fa78c5fc0
Brandon Jackson <jacksonbt@msoe.edu>
2015-02-19 19:04:20 -0600

    adds 2!

--- SE-3800InclassProj/SE-3800InclassProj/App/viewmodels/welcome.js
+++ SE-3800InclassProj/SE-3800InclassProj/App/viewmodels/welcome.js
@@ -13,7 +13,7 @@
             http.post('/api/Numbers?num=' + that.myNum).then(that.getNum)
         }
         this.postNum = function () {
-            that.myNum = that.myNum + 3;
+            that.myNum = that.myNum + 2;
             http.post('/api/Numbers?num=' + that.myNum).then(that.getNum)
             
             
